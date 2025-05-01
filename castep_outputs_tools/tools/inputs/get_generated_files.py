5d96969b4787ac8b70c76d447a984ba39fd5348d
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2025-05-01 14:23:53 +0100

    Add interface for get_generated_files

---
+++ castep_outputs_tools/tools/inputs/get_generated_files.py
@@ -0,0 +1,32 @@
+"""CLI Interface for getting generated files."""
+
+import argparse
+from pathlib import Path
+from textwrap import indent
+
+from castep_outputs.tools.get_generated_files import get_generated_files
+
+
+def get_parser():
+    """Get the argument parser for this script."""
+    arg_parser = argparse.ArgumentParser(
+        prog="castep_files",
+        description=("Attempts to work out what files will be produced "
+                     "from given seeds."),
+    )
+
+    arg_parser.add_argument(
+        "seedname", nargs=argparse.REMAINDER, type=Path, help="Seed name for data.",
+    )
+
+    return arg_parser
+
+
+def cli():
+    """Run from CLI."""
+    args = get_parser().parse_args()
+
+    for seed in args.seedname:
+        print(seed.stem)
+        out = "\n".join(map(str, get_generated_files(seed)))
+        print(indent(out, "- "))
